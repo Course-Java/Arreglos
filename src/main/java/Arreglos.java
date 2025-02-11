5d57782e1885a42dc3d0f83b5601ff1427ed3649
m4rcous <m.gongora.sanchez08@gmail.com>
2025-02-11 15:47:55 -0500

    feat: simplified array syntax

--- src/main/java/Arreglos.java
+++ src/main/java/Arreglos.java
@@ -1,17 +1,15 @@
 public class Arreglos {
     public static void main(String[] args) {
         // Arreglo
-        var enteros = new int[5];
+        //int[] enteros = {100, 200, 300, 400, 500};
+        var enteros = new int[]{100, 200, 300, 400, 500};
 
-        // Modificar los elementos
-        enteros[0] = 13;
-        enteros[1] = 21;
-        enteros[4] = 62;
+        for (var i = 0; i < enteros.length; i++) {
+            System.out.println("Valor " + (i + 1) + " : " + enteros[i]);
+        }
 
-        // Leer los elementos de un arreglo
-        System.out.println("Valor 1 = " + enteros[0]);
-        System.out.println("Valor 2 = " + enteros[1]);
-        System.out.println("Valor 5 = " + enteros[4]);
+        // Imprimir el arreglo -> nos da la dirección de memoria (ya que variable apunta a un arreglo que se encuentra en la memoria heap)
+        System.out.println("Imprimir el arreglo: " + enteros);
     }
 }
 
@@ -24,4 +22,7 @@ public class Arreglos {
  * Cada casilla del arreglo tienen asignadas un índice que puedes gestionar en los cuales puede almacenar variables del tipo asignado en la inicialización (por el momento se guardan el valor default de la variable (o sea 0 en el caso del int))
  * Recordemos que, por ejemplo, asignamos un espacio de 5 casillas, el índice irá desde 0 hasta n-1 (ya que incluimos el 0 cómo índice)
  * Recordemos que no podemos guardar valores fuera del rango de índices ya que nos saldría error de desbordamiento
+ * Hay formas de simplificar la sintaxis de la declaración de arreglos, cómo:
+ * int[] enteros = {100, 200, 300, 400, 500}; -> indicar el tipo de dato e indicar que es un arreglo y en la misma linea inicializar valores dentro de unas llaves (la cantidad de valores será la cantidad de espacio con la que se inicializa el arreglo)
+ * var enteros = new int[]{100, 200, 300, 400, 500}; -> usando var para luego declarar un arreglo de tipo int para luego, dentro de llaves, colocar los elementos
  */
